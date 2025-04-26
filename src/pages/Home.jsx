643a6bc107b170bcbc06159b79d97705c2d9b78f
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-04-26 23:59:23 +0400

    add_skills_and_education

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import myimg from "./img/myimg.png"
 import CV_EN from "../cv/CV_EN.pdf"
+import download from "./img/download.svg"
 import "../reset.css"
 
 const Home = () => {
@@ -8,45 +9,71 @@ const Home = () => {
     <section className="main_section flex flex-col p-[50px] gap-y-[50px]">
       <section className="name_photo flex justify-between gap-x-[50px]">
         <article className="name_position flex flex-col text-left">
-          <span className="text-[20px]">Front-End Developer</span>
+          <span className="text-[20px]">Full Stack develope</span>
           <article className="flex flex-col text-left">
             <span className="text-[150px]">Gaga</span>
             <span className="text-[150px] ml-[150px]">Chituashvili</span>
           </article>
         </article>
+        <article className="flex flex-col gap-y-[30px]">
         <img
           className="myimg h-[300px] object-cover"
           src={myimg}
           alt="My Portrait"
         />
-      </section>
-
-      <section className="cv_myskills">
-        <div className="flex flex-col items-center gap-4 p-6 bg-white shadow-md rounded-2xl max-w-md mx-auto">
-          <span className="text-xl font-semibold text-gray-800">Resume</span>
-          <a href={CV_EN} download="Gaga_Chituashvili_CV.pdf">
-            <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-6 rounded-lg transition">
-              Download CV
+         <a href={CV_EN} download="Gaga_Chituashvili_CV.pdf">
+            <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-6 rounded-lg transition flex items-center gap-x-[5px]">
+              Resume
+              <img className="w-6" src={download}/>
             </button>
           </a>
-        </div>
-        <article>
-          <span>skills</span>
-          <span>skills</span>
-          <span>skills</span>
-          <span>skills</span>
         </article>
       </section>
 
-      <section className="education">
-        <span>education</span>
-        <span>education</span>
-        <span>education</span>
-        <span>education</span>
-        <span>education</span>
+        <section className="h-[150px] bg-slate-200 flex justify-between items-center px-[30px]">
+          <span>Html & Css</span>
+          <span>Javascript</span>
+          <span>React JS</span>
+          <span>Python</span>
+          <span>Django</span>
+          <span>Tailwind Css</span>
+          <span>RESTful APIs</span>
+          <span>Git / GitHub</span>
+          <span>API Integration</span>
+        </section>
+
+
+      <section className="flex flex-col gap-y-[30px]">
+        <h2 className="font-bold text-[30px]">Education</h2>
+       <article className="flex flex-col gap-y-[20px] border border-solid border-black p-[30px]">
+       <span>Frontend Development-Python</span>
+        <span>Academy of Digital Industries</span>
+        <span>03/05/2025</span>
+       </article>
+       <article className="flex flex-col gap-y-[20px] border border-solid border-black p-[30px]">
+       <span>Frontend Development-React JS</span>
+        <span>Academy of Digital Industries</span>
+        <span>07/02/2025</span>
+       </article>
+       <article className="flex flex-col gap-y-[20px] border border-solid border-black p-[30px]">
+       <span>Frontend Development-Javascript</span>
+        <span>Academy of Digital Industries</span>
+        <span>12/12/2024</span>
+       </article>
+       <article className="flex flex-col gap-y-[20px] border border-solid border-black p-[30px]">
+       <span>Frontend Development-Html & Css</span>
+        <span>Academy of Digital Industries</span>
+        <span>19/08/2024</span>
+       </article>
+       <article className="flex flex-col gap-y-[20px] border border-solid border-black p-[30px]">
+       <span>Business Administration</span>
+        <span>Tbilisi State Uiversity</span>
+        <span>2019-2023</span>
+       </article>
       </section>
 
       <section className="project">
+        <h2>PROJECT</h2>
         <span>project1</span>
         <span>project2</span>
         <span>project3</span>
