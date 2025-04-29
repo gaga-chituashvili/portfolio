4bda355f1b067a17a010fa8484d67dc2565279b4
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-04-29 22:55:38 +0400

    add_experience

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -8,6 +8,13 @@ const Experience = () => {
       </h2>
       <article className="flex justify-center border border-black p-[30px] rounded-2xl hover:shadow-lg shadow-md shadow-slate-200 transition-shadow duration-300 bg-slate-200">
         <article className="flex flex-col items-center gap-5 text-center">
+          <span className="text-xl font-bold text-gray-800">Freelancer</span>
+          <span className="text-lg text-gray-600">Upwork</span>
+          <span className="text-base text-gray-500">2025 - Present</span>
+        </article>
+      </article>
+      <article className="flex justify-center border border-black p-[30px] rounded-2xl hover:shadow-lg shadow-md shadow-slate-200 transition-shadow duration-300 bg-slate-200">
+        <article className="flex flex-col items-center gap-5 text-center">
           <span className="text-xl font-bold text-gray-800">Manager</span>
           <span className="text-lg text-gray-600">Tegeta Motors</span>
           <span className="text-base text-gray-500">2023 - 2024</span>
