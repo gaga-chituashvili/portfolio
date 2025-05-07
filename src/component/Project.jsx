8a2218ee1ea45914c95d9972c8e1f0807f3c4e45
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-05-07 15:50:27 +0400

    change_django_adress

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -15,7 +15,7 @@ const Project = () => {
     window.open("https://kolhs.netlify.app", "_blank");
   };
   const geoRestourantClick = () => {
-    window.open("https://georgianrestaurant-diiwi.ondigitalocean.app", "_blank");
+    window.open("https://georestaurant-mch7f.ondigitalocean.app", "_blank");
   };
 
   return (
