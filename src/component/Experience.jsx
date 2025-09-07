d4a6aadbad9abb1a0691ff9f2644f37913f3a4d2
gaga-chituashvili <gagachituashvili7@gmail.com>
2025-09-08 00:14:41 +0400

    add_experience

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -8,7 +8,7 @@ const Experience = () => {
       </h2>
         <article className="flex justify-center border border-black p-[30px] rounded-2xl hover:shadow-lg shadow-md shadow-slate-200 transition-shadow duration-300 bg-slate-200">
         <article className="flex flex-col items-center gap-5 text-center">
-          <span className="text-xl font-bold text-gray-800">AI Developer</span>
+          <span className="text-xl font-bold text-gray-800">Back-End/AI Developer</span>
           <span className="text-lg text-gray-600">Breneo</span>
           <span className="text-base text-gray-500">2025 - Present</span>
         </article>
