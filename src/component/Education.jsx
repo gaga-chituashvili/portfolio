32d673216488fe8a69a76bf251732a2365727cc7
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-03-17 15:01:46 +0400

    extract education data into separate file

--- src/component/Education.jsx
+++ src/component/Education.jsx
@@ -1,57 +1,5 @@
 import React, { useState } from "react";
-import certificate1 from "./certificate/certificate1.pdf";
-import certificate2 from "./certificate/certificate2.pdf";
-import certificate3 from "./certificate/certificate3.pdf";
-import certificate4 from "./certificate/certificate4.pdf";
-import html_css from "./img/html_css.png";
-import js from "./img/js.png";
-import reactjs from "./img/reactjs.png";
-import python from "./img/python.png";
-
-const educationData = [
-  {
-    title: "Full Stack Development - Node JS",
-    school: "Academy of Digital Industries",
-    date: "17/03/2026",
-    img: python,
-    pdf: certificate4,
-  },
-  {
-    title: "Full Stack Development - Python",
-    school: "Academy of Digital Industries",
-    date: "02/05/2025",
-    img: python,
-    pdf: certificate4,
-  },
-  {
-    title: "Frontend Development - React JS",
-    school: "Academy of Digital Industries",
-    date: "07/02/2025",
-    img: html_css,
-    pdf: certificate3,
-  },
-  {
-    title: "Frontend Development - JavaScript",
-    school: "Academy of Digital Industries",
-    date: "12/12/2024",
-    img: js,
-    pdf: certificate2,
-  },
-  {
-    title: "Frontend Development - HTML & CSS",
-    school: "Academy of Digital Industries",
-    date: "19/08/2024",
-    img: reactjs,
-    pdf: certificate1,
-  },
-  {
-    title: "Business Administration",
-    school: "Tbilisi State University",
-    date: "2019 - 2023",
-    img: null,
-    pdf: null,
-  },
-];
+import educationData from "../data/education";
 
 const Education = () => {
   const [selectedPdf, setSelectedPdf] = useState(false);
---
+++ src/data/education.js
@@ -0,0 +1,55 @@
+import certificate1 from "../component/certificate/certificate1.pdf";
+import certificate2 from "../component/certificate/certificate2.pdf";
+import certificate3 from "../component/certificate/certificate3.pdf";
+import certificate4 from "../component/certificate/certificate4.pdf";
+import html_css from "../component/img/html_css.png";
+import js from "../component/img/js.png";
+import reactjs from "../component/img/reactjs.png";
+import python from "../component/img/python.png";
+
+const educationData = [
+  {
+    title: "Full Stack Development - Node JS",
+    school: "Academy of Digital Industries",
+    date: "17/03/2026",
+    img: python,
+    pdf: certificate4,
+  },
+  {
+    title: "Full Stack Development - Python",
+    school: "Academy of Digital Industries",
+    date: "02/05/2025",
+    img: python,
+    pdf: certificate4,
+  },
+  {
+    title: "Frontend Development - React JS",
+    school: "Academy of Digital Industries",
+    date: "07/02/2025",
+    img: html_css,
+    pdf: certificate3,
+  },
+  {
+    title: "Frontend Development - JavaScript",
+    school: "Academy of Digital Industries",
+    date: "12/12/2024",
+    img: js,
+    pdf: certificate2,
+  },
+  {
+    title: "Frontend Development - HTML & CSS",
+    school: "Academy of Digital Industries",
+    date: "19/08/2024",
+    img: reactjs,
+    pdf: certificate1,
+  },
+  {
+    title: "Business Administration",
+    school: "Tbilisi State University",
+    date: "2019 - 2023",
+    img: null,
+    pdf: null,
+  },
+];
+
+export default educationData;
