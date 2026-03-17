0b503f831b158261cb231b13cb8c9cecfe6289b9
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-03-17 15:10:36 +0400

    extract social data into separate file

--- src/component/Social.jsx
+++ src/component/Social.jsx
@@ -1,28 +1,5 @@
 import React from "react";
-import { Linkedin, Github, Instagram, Facebook } from "lucide-react";
-
-const socials = [
-  {
-    name: "LinkedIn",
-    icon: <Linkedin className="text-blue-500 w-10 h-10" />,
-    url: "https://www.linkedin.com/in/gaga-chituashvili-222551320/",
-  },
-  {
-    name: "GitHub",
-    icon: <Github className="text-gray-300 w-10 h-10" />,
-    url: "https://github.com/gaga-chituashvili",
-  },
-  {
-    name: "Instagram",
-    icon: <Instagram className="text-pink-800 w-10 h-10" />,
-    url: "https://www.instagram.com/gagachituashvilii/",
-  },
-  {
-    name: "Facebook",
-    icon: <Facebook className="text-blue-600 w-10 h-10" />,
-    url: "https://www.facebook.com/profile.php?id=100056927246075",
-  },
-];
+import { socials } from "../data/social";
 
 const Social = () => {
   return (
---
+++ src/data/social.js
@@ -0,0 +1,24 @@
+import { Linkedin, Github, Instagram, Facebook } from "lucide-react";
+
+export const socials = [
+  {
+    name: "LinkedIn",
+    icon: <Linkedin className="text-blue-500 w-10 h-10" />,
+    url: "https://www.linkedin.com/in/gaga-chituashvili-222551320/",
+  },
+  {
+    name: "GitHub",
+    icon: <Github className="text-gray-300 w-10 h-10" />,
+    url: "https://github.com/gaga-chituashvili",
+  },
+  {
+    name: "Instagram",
+    icon: <Instagram className="text-pink-800 w-10 h-10" />,
+    url: "https://www.instagram.com/gagachituashvilii/",
+  },
+  {
+    name: "Facebook",
+    icon: <Facebook className="text-blue-600 w-10 h-10" />,
+    url: "https://www.facebook.com/profile.php?id=100056927246075",
+  },
+];
